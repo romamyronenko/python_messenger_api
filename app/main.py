e37e037485c749ff7942755c37fcabea081cb8c7
romamyronenko123 <eoma575@gmail.com>
2024-10-04 00:01:15 +0300

    black formating, update /docs, change auth to json, remove password from /auth/users/me

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from starlette.testclient import TestClient
 
 import database.schema
 from app.authorization import auth_router
-from app.models import Message
 from app.security import get_current_user
-from database import engine
+from models import models
+from models.database import engine
+from models.message import Message
 
-database.schema.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -29,21 +30,17 @@ app.add_middleware(
 def home():
     return {"hello": "world"}
 
-
 @app.post("/chat/{chat_id}")
 def send_message(chat_id: int, message: Message, user: str = Depends(get_current_user)):
     pass
-
 
 @app.get("/chat/{chat_id}")
 def get_messages(chat_id: int, user: str = Depends(get_current_user)):
     pass
 
-
 @app.get("/contacts")
 def get_contacts(user: str = Depends(get_current_user)):
     pass
-
 
 @app.post("/chat")
 def create_chat(user: str = Depends(get_current_user)):
