258f5d2dd72656571960b167834c851de5146fb2
romamyronenko123 <eoma575@gmail.com>
2024-10-04 00:01:15 +0300

    black formating, update /docs, change auth to json, remove password from /auth/users/me

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, Depends, WebSocket
 from starlette.middleware.cors import CORSMiddleware
 from starlette.testclient import TestClient
-
-import database.schema
 from app.authorization import auth_router
-from app.models import Message
 from app.security import get_current_user
-from database import engine
+from models import models
+from models.database import engine
+from models.message import Message
 
-database.schema.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -29,36 +28,22 @@ app.add_middleware(
 def home():
     return {"hello": "world"}
 
-
 @app.post("/chat/{chat_id}")
 def send_message(chat_id: int, message: Message, user: str = Depends(get_current_user)):
     pass
-
 
 @app.get("/chat/{chat_id}")
 def get_messages(chat_id: int, user: str = Depends(get_current_user)):
     pass
 
-
 @app.get("/contacts")
 def get_contacts(user: str = Depends(get_current_user)):
     pass
-
 
 @app.post("/chat")
 def create_chat(user: str = Depends(get_current_user)):
     pass
 
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import uvicorn
-
     uvicorn.run("main:app")
