75e967579f5fffcfb6d70ec0416550beaee50f10
romamyronenko123 <eoma575@gmail.com>
2024-10-04 00:01:15 +0300

    black formating, update /docs, change auth to json, remove password from /auth/users/me

--- app/security.py
+++ app/security.py
@@ -76,9 +76,7 @@ def get_db():
         db.close()
 
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
