792fd5fd1d2ab6708d3d6184ca028dd6de3a5ccc
andrii <andrewmagalyas@gmail.com>
2024-10-04 20:10:50 +0200

    initial commit

--- database/schema.py
+++ database/schema.py
@@ -1,15 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    Boolean,
-    TIMESTAMP,
-    ForeignKey,
-    DateTime,
-)
-from sqlalchemy.dialects.mysql import VARCHAR
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import current_timestamp
