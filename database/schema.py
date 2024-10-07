71325fbc917e213b8c0f384f4841380407c15775
andrii <andrewmagalyas@gmail.com>
2024-10-07 23:51:24 +0200

    Changes according to tests

--- database/schema.py
+++ database/schema.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     Boolean,
     TIMESTAMP,
     ForeignKey,
+    DateTime,
 )
 from sqlalchemy.dialects.mysql import VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
