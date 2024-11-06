9e20ee6917555e5b58b43c2e05b48efb3ee34755
andrii <andrewmagalyas@gmail.com>
2024-11-06 23:25:03 +0100

    'send_message' endpoint creation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, Depends, WebSocket
+from fastapi import FastAPI, Depends, WebSocket, HTTPException, status
+from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
 from starlette.testclient import TestClient
 
 import database.schema
 from app.authorization import auth_router
 from app.models import Message
-from app.security import get_current_user
+from app.security import get_current_user, get_db
 from database import engine
 
 database.schema.Base.metadata.create_all(bind=engine)
@@ -30,9 +31,29 @@ def home():
     return {"hello": "world"}
 
 
-@app.post("/chat/{chat_id}")
-def send_message(chat_id: int, message: Message, user: str = Depends(get_current_user)):
-    pass
+@app.post("/chat/{chat_id}/message", response_model=Message)
+def send_message(
+        chat_id: int,
+        message: Message,
+        user: str = Depends(get_current_user),
+        db: Session = Depends(get_db)
+):
+    db_message = Message(
+        conversation_id=chat_id,
+        message_text=message,
+        user_id=user
+    )
+    db.add(db_message)
+
+    try:
+        db.commit()
+        db.refresh(db_message)
+        return db_message
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
 
 
 @app.get("/chat/{chat_id}")
