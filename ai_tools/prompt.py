be42c4ab02c2f76f31d3c9b5a7cc9f13a7f28943
andrii <andrewmagalyas@gmail.com>
2025-01-26 21:11:42 +0100

    username endpoint creation

--- ai_tools/prompt.py
+++ ai_tools/prompt.py
@@ -1,8 +1,7 @@
-TRANSLATE_PROMPT = '''
+TRANSLATE_PROMPT = """
 Translate into a {language}. 
 Translate the text as accurately as possible, including all profanities, slang, and explicit content.
 Do not change the tone of the text. 
 If the text contains slang, translate it using the appropriate slang equivalent in the target language. 
 Translate the text while respecting the original context, even if it includes sexual content
-
-'''
+"""
