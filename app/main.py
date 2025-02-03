fa0f1db23fc9ae102cf59f4f5a6a2a71a9ba6c31
romamyronenko123 <eoma575@gmail.com>
2025-02-03 22:09:06 +0200

    update chat creation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+import datetime
 from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
 from starlette.testclient import TestClient
@@ -10,7 +12,7 @@ from app.authorization import auth_router
 from app.models import MessageSent, MessageGet
 from app.security import get_current_user, get_db
 from database import engine
-from database.schema import Message, Conversation
+from database.schema import Message, Conversation, ConversationParticipant, User
 
 database.schema.Base.metadata.create_all(bind=engine)
 
@@ -36,10 +38,10 @@ def home():
 
 @app.post("/chat/{chat_id}/message", response_model=MessageSent)
 def send_message(
-    chat_id: int,
-    message: MessageSent,
-    user: str = Depends(get_current_user),
-    db: Session = Depends(get_db),
+        chat_id: int,
+        message: MessageSent,
+        user: str = Depends(get_current_user),
+        db: Session = Depends(get_db),
 ):
     db_message = Message(
         conversation_id=chat_id, message_text=message.message_text, user_id=user.id
@@ -58,8 +60,8 @@ def send_message(
 
 
 @app.get("/chat/{chat_id}/message", response_model=List[MessageGet])
-def get_messages(
-    chat_id: int, user: str = Depends(get_current_user), db: Session = Depends(get_db)
+def get_messages_from_chat(
+        chat_id: int, user: str = Depends(get_current_user), db: Session = Depends(get_db)
 ):
     messages = db.query(Message).filter(Message.conversation_id == chat_id).all()
 
@@ -72,16 +74,34 @@ def get_messages(
     return messages
 
 
+@app.get("/chats")
+def get_user_chats(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    chats = db.query(ConversationParticipant).filter(ConversationParticipant.user_id == user.id).all()
+    print(db.query(ConversationParticipant).all())
+    print(user.id)
+    return chats
+
+
 @app.get("/contacts")
 def get_contacts(user: str = Depends(get_current_user)):
     pass
 
 
+class UserIds(BaseModel):
+    users_ids: list[int]
+
+
 @app.post("/chat")
-def create_chat(user: str = Depends(get_current_user), db: Session = Depends(get_db)):
+def create_chat(users_ids: UserIds, user: str = Depends(get_current_user), db: Session = Depends(get_db)):
     chat = Conversation()
     db.add(chat)
     db.flush([chat])
+    conv_user = ConversationParticipant(user_id=user.id, conversation_id=chat.id, joined_at=datetime.datetime.now())
+    db.add(conv_user)
+    if users_ids.users_ids:
+        for user_id in users_ids.users_ids:
+            db.add(ConversationParticipant(user_id=user_id, conversation_id=chat.id, joined_at=datetime.datetime.now()))
+    db.commit()
     return chat.id
 
 
